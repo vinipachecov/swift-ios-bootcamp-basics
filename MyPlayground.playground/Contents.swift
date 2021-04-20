c8e43407ac4421ce10ff7a40fe3ed3a0834c8afb
Vinicius Vieira <vinipachecov@gmail.com>
2021-04-20 19:21:36 -0300

    feat: add string interpolation and variables basics

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Printing normal text
+//print("Hello, playground")
+
+// Single comment line
+
+/**
+ Multiline comments
+ */
+
+// String interpolation
+print("Hello, \(2+2) playground")
+
+
+
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var a = 5
+
+var b = 8
+
+
+print("a \(a)")
+print("b \(b)")

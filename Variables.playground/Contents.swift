4818a27af12ee47a21e595d5b08af1673ddae95b
Vinicius Vieira <vinipachecov@gmail.com>
2021-04-24 16:31:15 -0300

    types of variables in swift

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,9 +1,19 @@
 import UIKit
 
+// variables that can change the value
 var a = 5
-
 var b = 8
-
 
 print("a \(a)")
 print("b \(b)")
+
+a = 15
+
+print("updated value of a = \(a)")
+
+// let is the keyword for constant variables in Swift
+let constantValue = "hello"
+
+// this will lead to an error
+//constantValue = "new value"
+print (constantValue)

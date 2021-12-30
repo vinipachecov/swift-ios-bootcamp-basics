f0b16f91565580403cfb8b135c53e5a90ea0b3a3
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-12-30 19:06:16 -0300

    A swift UI app that lists news in a list and renders a webview

---
+++ H4X0R News/H4X0R News/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by vinicius p  vieira on 28/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkmanager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkmanager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkmanager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+/* let posts = [
+ Post(id: "1", title: "Hello"),
+ Post(id: "2", title: "Hola"),
+ Post(id: "3", title: "Hello")
+ ]
+ */
---
+++ H4X0R News/H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by vinicius p  vieira on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
